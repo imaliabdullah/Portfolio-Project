43a0f93baa95283476b8d7bfefb3962fac3eecbe
imaliabdullah <aliabdullah4545@gmail.com>
2024-03-18 15:19:38 +0500

    Create api.py

---
+++ iris_project/api.py
@@ -0,0 +1,39 @@
+import flask
+import json
+import numpy as np
+from joblib import dump, load
+from flask import Flask, render_template, request, jsonify
+
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/index")
+def index():
+	return flask.render_template('index.html')
+
+@app.route("/predict", methods=['POST'])
+def make_predictions():
+        if request.method =='POST':
+                sl = request.form.get('sepal-length')
+                sw = request.form.get('sepal-width')
+                pl = request.form.get('petal-length')
+                pw = request.form.get('petal-width')
+                X = np.array([sl,sw,pl,pw]).reshape(1,-1)
+                prediction = model.predict(X)
+
+                resultText = json.dumps(prediction.tolist(),sort_keys = False)
+                resultTextObject = json.loads(resultText)
+                # This json.dumps will convert 'prediction' to JSON
+
+                return render_template('predictPage.html',response=resultTextObject[0])
+                # 'response' is a Jinja2 variable embedded in predictPage.html
+
+@app.route('/api')
+def hello():
+    response = {'MESSAGE':'Welcome to the new API route'}
+    return jsonify(response)
+
+if __name__ == '__main__':
+    model = load('model.pkl')
+    app.run(host='0.0.0.0', port=8000, debug=True)
